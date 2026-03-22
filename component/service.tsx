dffe7ff7810e32fa2ed4d6649d59c7d2fe261477
aman choudhary <aman8234c@gmail.com>
2026-03-22 17:26:17 +0530

    service page styling changes

--- component/service.tsx
+++ component/service.tsx
@@ -3,7 +3,7 @@ import { homeServiceSection } from "@/constant/constant";
 export default function Service() {
   return (
     <div className="p-10">
-      <h2 className="highlightFont text-white text-5xl md:text-9xl font-bold my-2">
+      <h2 className="highlightFont text-white text-6xl md:text-9xl font-bold my-2">
         SERVICES
         <span className="  text-(--highlightColor)">.</span>
       </h2>
@@ -12,15 +12,15 @@ export default function Service() {
           return (
             <div
               key={index}
-              className="py-10  flex justify-between items-center  border-b relative group"
+              className="py-10  flex justify-between items-center flex-wrap  border-b relative group"
             >
-              <div className="flex items-center">
-                <p className="w-10 text-3xl text-(--highlightColor) mr-10 font-medium">{`0${index + 1}`}</p>
-                <h2 className="w-100 text-3xl text-white font-medium">
+              <div className="flex items-center mb-5 lg:mb-0">
+                <p className="w-10 text-3xl text-(--highlightColor) mr-2 sm:mr-10 font-medium">{`0${index + 1}`}</p>
+                <h2 className="w-full sm:w-60  lg:w-100  sm:text-3xl text-white font-medium">
                   {service.title}
                 </h2>
               </div>
-              <p className="w-100">{service.paragraph}</p>
+              <p className="w-full sm:w-60  lg:w-100">{service.paragraph}</p>
               <div className="w-0 h-0.5 absolute -bottom-0.5 left-0  bg-(--highlightColor) transition-all duration-500 group-hover:w-full z-10"></div>
               <div className="rotate-180 w-0 h-0.5 absolute -bottom-0.5 right-0 bg-(--highlightColor) transition-all  duration-500 group-hover:w-full z-10"></div>
             </div>
