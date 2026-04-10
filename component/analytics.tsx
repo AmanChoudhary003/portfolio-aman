e4cb0f88db1477f2ec8c8460043eb81e09dba0a0
aman choudhary <aman8234c@gmail.com>
2026-04-10 11:49:31 +0530

    error fix in analytics

--- component/analytics.tsx
+++ component/analytics.tsx
@@ -1,16 +1,19 @@
 import Script from "next/script";
 
 export default function Analytics() {
+  const gaID = process.env.G_ID;
+  if (!gaID) return null;
+
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.G_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaID}`}
         strategy="afterInteractive"
       ></Script>
-      <Script>
-        {`window.dataLayer = window.dataLayer || []; function gtag()
-        {window.dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', ${process.env.G_ID})`}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || []; 
+        function gtag() {window.dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', '${gaID}')`}
       </Script>
     </>
   );
